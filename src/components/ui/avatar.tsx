c912912f2945e153db9b552b602fa065ef673a45
meronmks <meronmks.8914@gmail.com>
2025-04-27 19:17:57 +0900

    fix: 環境によっては画像の表示が壊れるのを修正

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,7 +2,7 @@ export function Avatar({ src, className }: { src: string; className?: string }) 
     return (
       <div className="avatar">
         <div className={`rounded-full ${className}`}>
-          <img src={src} alt="avatar" />
+          <img src={src} alt="avatar" className="w-full h-full object-cover" />
         </div>
       </div>
     );
